e9a1649ea2a23ae31f6dc1f0f97a78f1a4170cdd
Oskar <oskar.pala92@gmail.com>
2025-08-04 13:44:23 +0200

    created DTO's and method for first request

--- src/main/java/com/recruitmenttask/reporetriever/proxy/GitHubApiProxy.java
+++ src/main/java/com/recruitmenttask/reporetriever/proxy/GitHubApiProxy.java
@@ -1,4 +1,17 @@
 package com.recruitmenttask.reporetriever.proxy;
 
-interface GitHubApiProxy {
+import com.recruitmenttask.reporetriever.proxy.dto.GitHubRepositoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+//Repository Name
+//Owner Login
+//For each branch it’s name and last commit sha
+
+@FeignClient("https://api.github.com/")
+public interface GitHubApiProxy {
+    @GetMapping("users/{userName}/repos")
+    List<GitHubRepositoryDto> getAllRepos(@PathVariable("userName") String userName);
 }
---
+++ src/main/java/com/recruitmenttask/reporetriever/proxy/dto/OwnerDto.java
@@ -0,0 +1,4 @@
+package com.recruitmenttask.reporetriever.proxy.dto;
+
+record OwnerDto() {
+}
