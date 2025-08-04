b97d8b3b97e67405239c5358cdf383b0c78bc600
Oskar <oskar.pala92@gmail.com>
2025-08-04 15:03:37 +0200

    created repository retriever

---
+++ src/main/java/com/recruitmenttask/reporetriever/service/RepositoryRetriever.java
@@ -0,0 +1,33 @@
+package com.recruitmenttask.reporetriever.service;
+
+import com.recruitmenttask.reporetriever.proxy.GitHubApiProxy;
+import com.recruitmenttask.reporetriever.proxy.dto.BranchDto;
+import com.recruitmenttask.reporetriever.proxy.dto.GitHubRepositoryDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+class RepositoryRetriever {
+    private final GitHubApiProxy gitHubApiProxy;
+
+    List<GitHubRepositoryDto> getFilteredRepositories(String username) {
+        return makeGetRepositoryRequest(username).stream()
+                .filter(gitHubRepositoryDto -> !gitHubRepositoryDto.fork())
+                .collect(Collectors.toList()
+                );
+    }
+
+    List<GitHubRepositoryDto> makeGetRepositoryRequest(String username) {
+        return gitHubApiProxy.getAllRepos(username);
+    }
+
+    List<BranchDto> makeBranchesRequest(String owner, String repoName) {
+        return gitHubApiProxy.getAllBranches(owner, repoName);
+    }
+
+    RepositoryRetriever(final GitHubApiProxy gitHubApiProxy) {
+        this.gitHubApiProxy = gitHubApiProxy;
+    }
+}
